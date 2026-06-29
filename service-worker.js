289013498d6cfdf94990d78bfd285e58c85d47c4
AwReel-Ink <awreeldroid@gmail.com>
2026-06-29 21:53:53 +0200

    Update service-worker.js1.01

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * Stratégie: Cache First pour les assets locaux
  */
 
-const CACHE_NAME = 'sumattra-v1';
+const CACHE_NAME = 'sumattra-v1.01';
 const ASSETS_TO_CACHE = [
   './index.html',
   './css/style.css',
